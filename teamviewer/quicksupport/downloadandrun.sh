384e1167eae9794025c6ea450fde1c185f3b6454
Raul Tierno <raultm@gmail.com>
2020-04-28 10:17:27 +0200

    Update downloadandrun.sh

--- teamviewer/quicksupport/downloadandrun.sh
+++ teamviewer/quicksupport/downloadandrun.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-# bash <(curl -s https://raw.githubusercontent.com/iesextremadura/commonsolutions/teamviewer/quicksupport/downloadandrun.sh)
+# bash <(curl -s https://raw.githubusercontent.com/iesextremadura/commonsolutions/master/teamviewer/quicksupport/downloadandrun.sh)
 
 # Download
-# wget https://github.com/iesextremadura/commonsolutions/blob/master/teamviewer/quicksupport/teamviewer_15.4.4445_amd64.tar.xz
+echo "Descargando paquete de TeamViewer"
+wget https://github.com/iesextremadura/commonsolutions/blob/master/teamviewer/quicksupport/teamviewer_15.4.4445_amd64.tar.xz
 
 # Uncompress & Run
-# ...
+echo "Descomprimiendo el paquete descargado"
+tar -xvf teamviewer_15.4.4445_amd64.tar.xz
 
-echo "Hola"
+echo "Ejecutando"
+
