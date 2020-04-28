ef06a1fe8190e7072ae110674b25a8a4da0c595d
Raul Tierno <raultm@gmail.com>
2020-04-28 10:13:03 +0200

    Update downloadandrun.sh

--- teamviewer/quicksupport/downloadandrun.sh
+++ teamviewer/quicksupport/downloadandrun.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+# bash <(curl -s https://raw.githubusercontent.com/iesextremadura/commonsolutions/teamviewer/quicksupport/downloadandrun.sh)
 
 # Download
 # wget https://github.com/iesextremadura/commonsolutions/blob/master/teamviewer/quicksupport/teamviewer_15.4.4445_amd64.tar.xz
 
 # Uncompress & Run
 # ...
+
+echo "Hola"
